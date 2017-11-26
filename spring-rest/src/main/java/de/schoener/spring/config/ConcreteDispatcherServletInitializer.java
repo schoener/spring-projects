09b2a2edead9a64e5c21b3ee614109685a7999d1
Vladimir Schöner <Wowtschikus@gmx.net>
2017-11-26 12:39:34 +0100

    added missing files

---
+++ spring-rest/src/main/java/de/schoener/spring/config/ConcreteDispatcherServletInitializer.java
@@ -0,0 +1,29 @@
+package de.schoener.spring.config;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class ConcreteDispatcherServletInitializer extends
+		AbstractDispatcherServletInitializer {
+
+	@Override
+	protected WebApplicationContext createServletApplicationContext() {
+		XmlWebApplicationContext context = new XmlWebApplicationContext();
+		context.setConfigLocation("classpath:spring/mvc-dispatcher-servlet.xml");
+		return context;
+	}
+
+	@Override
+	protected WebApplicationContext createRootApplicationContext() {
+		XmlWebApplicationContext context = new XmlWebApplicationContext();
+		context.setConfigLocation("classpath:spring/contextConfig.xml");
+		return context;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
