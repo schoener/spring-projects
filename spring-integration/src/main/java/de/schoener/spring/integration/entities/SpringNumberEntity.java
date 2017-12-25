cd5a15c510de042f9070f67f093ae6f8974b1d06
Vladimir Schöner <Wowtschikus@gmx.net>
2017-12-25 22:11:26 +0100

    added sequence generator

--- spring-integration/src/main/java/de/schoener/spring/integration/entities/SpringNumberEntity.java
+++ spring-integration/src/main/java/de/schoener/spring/integration/entities/SpringNumberEntity.java
@@ -3,9 +3,11 @@ package de.schoener.spring.integration.entities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -19,7 +21,8 @@ public class SpringNumberEntity {
 	public static final String FIND_NUMBERS = "SpringNumberEntity.findNumbers";
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(generator = "SPRINGNUMBER_SEQUENCE_GENERATOR", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "SPRINGNUMBER_SEQUENCE_GENERATOR", sequenceName = "SPRINGNUMBER_SEQUENCE", allocationSize = 1)
 	@Column(name = "ID")
 	private Long id;
 
