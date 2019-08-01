98c8161d2a927ee575e264d98f85782f7615b9ad
Vladimir Schöner <Wowtschikus@gmx.net>
2019-08-01 18:42:59 +0200

    organized imports

--- spring-rest/src/main/java/de/schoener/spring/config/WebMvcConfiguration.java
+++ spring-rest/src/main/java/de/schoener/spring/config/WebMvcConfiguration.java
@@ -2,7 +2,6 @@ package de.schoener.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import de.schoener.spring.PackageScanner;
