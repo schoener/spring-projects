cc90882214933d3d2b8aa227dd7d75fd9fe04499
Vladimir Schöner <Wowtschikus@gmx.net>
2019-08-06 22:18:11 +0200

    added jstl support added home and spittles jsp page

--- spring-rest/src/main/java/de/schoener/spring/config/WebMvcConfiguration.java
+++ spring-rest/src/main/java/de/schoener/spring/config/WebMvcConfiguration.java
@@ -1,12 +1,35 @@
 package de.schoener.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import de.schoener.spring.PackageScanner;
 
 @Configuration
 @ComponentScan(basePackageClasses = { PackageScanner.class })
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+		resolver.setExposeContextBeansAsAttributes(true);
+		return resolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		super.addResourceHandlers(registry);
+	}
+
 }
--- spring-rest/src/main/java/de/schoener/spring/rest/controller/SpittleController.java
+++ spring-rest/src/main/java/de/schoener/spring/rest/controller/SpittleController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TestRestController {
-	@GetMapping(value = "/")
+public class SpittleController {
+	@GetMapping(value = "/spittles")
 	public String home() {
-		return "home";
+		return "spittles";
 	}
+
 }
